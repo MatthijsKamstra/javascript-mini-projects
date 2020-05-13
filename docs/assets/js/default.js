917b6d9140535eb8e656edd70dd367e223b5022b
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-05-13 19:40:53 +0200

    WIP todo

--- docs/assets/js/default.js
+++ docs/assets/js/default.js
@@ -8,7 +8,7 @@ class Main {
 		this.homeUrl = "../_nav.html";
 		this.INFO_ID = "mySidenav";
 		this.HOME_ID = "myHomeSideNav";
-		window.console.log("" + App.NAME + " - Navigation - Dom ready :: build: " + "2020-05-12 17:18:31");
+		window.console.log("" + App.NAME + " - Navigation - Dom ready :: build: " + "2020-05-13 15:49:51");
 		this.loadData(this.homeUrl,$bind(this,this.setupHome));
 		this.loadData(this.infoUrl,$bind(this,this.setupInfo));
 		if(window.document.getElementById("homepage") != null) {

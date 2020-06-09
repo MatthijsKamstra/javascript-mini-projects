ec21b1f548346db9f1569330437aab6cfc93e5ef
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-06-09 19:57:42 +0200

    working on shoppingcard

--- docs/shoppingcard/shoppingcard.js
+++ docs/shoppingcard/shoppingcard.js
@@ -2,10 +2,89 @@
 (function ($global) { "use strict";
 class Shoppingcard {
 	constructor() {
-		console.log("src/Shoppingcard.hx:3:","Shoppingcard");
+		this.sideNavInnerWidth = 740;
+		this.map = new haxe_ds_StringMap();
+		this.json = "coffee.json";
+		let _gthis = this;
+		window.document.addEventListener("DOMContentLoaded",function(event) {
+			$global.console.log("Shoppingcard -Dom ready");
+			_gthis.init();
+			_gthis.setElements();
+		});
+	}
+	setElements() {
+		this.sideNav = window.document.getElementById("shoppingnav");
+		let btn = window.document.getElementById("btn-sidenav-close");
+		let _gthis = this;
+		btn.onclick = function(e) {
+			e.preventDefault();
+			_gthis.toggleNav();
+		};
+		let btn1 = window.document.getElementById("btn-sidenav-open");
+		btn1.onclick = function(e) {
+			e.preventDefault();
+			_gthis.toggleNav();
+		};
+	}
+	init() {
+		let _gthis = this;
+		this.vm = new Vue({ el : "#app", data : { message : "Hello Vue.js!", name : "Vue.js", count : 20, json : { }}, methods : { greet : function(event) {
+			event.preventDefault();
+			_gthis.test(event);
+		}}});
+		this.loadData(this.json,$bind(this,this.setupJsonData));
+	}
+	test(e) {
+		$global.console.log(e);
+		let link = e.currentTarget;
+		console.log("src/Shoppingcard.hx:69:",link.dataset.id);
+		let coffeeObj = this.map.h[link.dataset.id];
+		$global.console.log(coffeeObj.name);
+	}
+	setupJsonData(data) {
+		let _json = JSON.parse(data);
+		let arr = _json.items;
+		let _g = 0;
+		let _g1 = arr.length;
+		while(_g < _g1) {
+			let i = _g++;
+			let coffee = arr[i];
+			this.map.h[coffee.guid] = coffee;
+		}
+		this.vm.$data.json = _json;
+	}
+	loadData(url,callback) {
+		let req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+			let tmp = this.readyState == 4 && this.status == 200;
+		};
+		req.onload = function() {
+			let body = req.response;
+			if(this.status == 200) {
+				callback(body);
+			}
+		};
+		req.onerror = function(error) {
+			$global.console.error("[JS] error: " + error);
+		};
+		req.open("GET",url);
+		req.send();
+	}
+	toggleNav() {
+		console.log("src/Shoppingcard.hx:127:","toggleNav " + this.sideNav.style.left);
+		if(this.sideNav.style.left != "-" + this.sideNavInnerWidth + "px") {
+			this.sideNav.style.left = "-" + this.sideNavInnerWidth + "px";
+		} else {
+			this.sideNav.style.left = "0";
+		}
 	}
 	static main() {
 		let app = new Shoppingcard();
+	}
+}
+class haxe_ds_StringMap {
+	constructor() {
+		this.h = Object.create(null);
 	}
 }
 class haxe_iterators_ArrayIterator {
@@ -20,5 +99,8 @@ class haxe_iterators_ArrayIterator {
 		return this.array[this.current++];
 	}
 }
+var $_;
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $global.$haxeUID++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = m.bind(o); o.hx__closures__[m.__id__] = f; } return f; }
+$global.$haxeUID |= 0;
 Shoppingcard.main();
-})({});
+})(typeof window != "undefined" ? window : typeof global != "undefined" ? global : typeof self != "undefined" ? self : this);
--- src/Shoppingcard.hx
+++ src/Shoppingcard.hx
@@ -1,9 +1,148 @@
+package;
+
+import haxe.ds.StringMap;
+import AST.ProjectObj;
+import vue.Vue;
+import haxe.Json;
+import haxe.Constraints.Function;
+import js.Lib;
+import js.Browser.*;
+import js.html.XMLHttpRequest;
+
 class Shoppingcard {
+	var json = "coffee.json";
+	var map:Map<String, CoffeeObj> = [];
+	var vm:Vue;
+	var sideNav:DivElement;
+	var sideNavOuterWidth = 800;
+	var sideNavThingWidth = 60;
+	var sideNavInnerWidth = 800 - 60;
+
 	public function new() {
-		trace('Shoppingcard');
+		document.addEventListener("DOMContentLoaded", function(event) {
+			console.log('Shoppingcard -Dom ready');
+			init();
+			setElements();
+		});
+	}
+
+	function setElements() {
+		sideNav = cast document.getElementById('shoppingnav');
+
+		var btn = cast document.getElementById('btn-sidenav-close');
+		btn.onclick = (e:MouseEvent) -> {
+			e.preventDefault();
+			toggleNav();
+		}
+		var btn = cast document.getElementById('btn-sidenav-open');
+		btn.onclick = (e:MouseEvent) -> {
+			e.preventDefault();
+			toggleNav();
+		}
+	}
+
+	function init() {
+		// set vm dom
+		vm = new Vue({
+			el: '#app',
+			data: {
+				message: 'Hello Vue.js!',
+				name: 'Vue.js',
+				count: 20,
+				json: {}
+			},
+			methods: {
+				greet: function(event:MouseEvent) {
+					event.preventDefault();
+					test(event);
+					// alert('Hello ' + Lib.nativeThis.name + '!');
+				}
+			}
+		});
+		loadData(json, setupJsonData);
+	}
+
+	function test(e:MouseEvent) {
+		console.log(e);
+
+		var link:AnchorElement = cast e.currentTarget;
+		trace(link.dataset.id);
+		// console.log('test  $e');
+
+		var coffeeObj:CoffeeObj = map.get(link.dataset.id);
+		console.log(coffeeObj.name);
+	}
+
+	/**
+		description:
+		img:
+		intensity:
+		name:
+		price:
+
+		* @param data
+	 */
+	function setupJsonData(data:String) {
+		var _json = Json.parse(data);
+		// var arr:Array<ProjectObj> = _json.data;
+		// vm.data.count = arr.length;
+		// trace(_json.items);
+		var arr:Array<CoffeeObj> = _json.items;
+		for (i in 0...arr.length) {
+			var coffee:CoffeeObj = arr[i];
+			map.set(coffee.guid, coffee);
+		}
+		vm.data.json = _json;
+	}
+
+	/**
+	 * loading data, check if file exists before generating navigation
+	 * @param url 		load file from here
+	 * @param callback 	if file exists and is loaded, use this callbakc
+	 */
+	function loadData(url:String, callback:Function) {
+		var req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+			if (Lib.nativeThis.readyState == 4 && Lib.nativeThis.status == 200) {
+				// console.log(Lib.nativeThis.responseText);
+				// finishSetup();
+			}
+		};
+		req.onload = function() {
+			var body = req.response;
+
+			if (Lib.nativeThis.status == 200)
+				callback(body);
+		};
+		req.onerror = function(error) {
+			console.error('[JS] error: $error');
+		};
+		req.open('GET', url);
+		req.send();
+	}
+
+	// ____________________________________ nav ____________________________________
+
+	function toggleNav() {
+		trace('toggleNav ${sideNav.style.left}');
+		if (sideNav.style.left != '-${sideNavInnerWidth}px') {
+			sideNav.style.left = '-${sideNavInnerWidth}px';
+		} else {
+			sideNav.style.left = "0";
+		}
 	}
 
 	static public function main() {
 		var app = new Shoppingcard();
 	}
+}
+
+typedef CoffeeObj = {
+	@:optional var _id:String;
+	var guid:String; // Vc0rzoKc1pJM4084oZmxb6dPzClcZ3wA
+	var name:String; // Vivalto
+	var img:String; // img/front_vivalto_lungo_decaf_Striped.png",
+	var description:String; // Witha full body and light coffee
+	var intensity:Int; // 5
+	var price:Float; // 0.51
 }
